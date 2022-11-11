8597b3507f46aac482f2a1dd8826b653cb2eb1e8
x10102 <x10102.ot@gmail.com>
2022-11-11 14:29:20 +0100

    Funguje to joo

--- Editor.py
+++ Editor.py
@@ -1,12 +1,21 @@
-from glob import glob
 import json
+from unicodedata import name
 from colorama import init, Fore, Back
 from os import system
 from prettytable import PrettyTable
+import re
 
 filename_default = "translations.json"
 db = {}
 selected_user = ""
+
+def write_db():
+    try:
+        with open(filename_default, "w", encoding="utf-8") as dbfile:
+            json.dump(db, dbfile)
+    except IOError as e:
+        print(Fore.RED + "Chyba při zápisu: " + str(e) + Fore.RESET)
+
 
 def print_kv(key, value, color):
     print(f"{key}: {color}{value}{Fore.RESET}{Back.RESET}")
@@ -37,12 +46,15 @@ def user_menu():
     user = db[selected_user]
 
     def print_user_menu():
+        last_article = list(user['articles'].keys())[-1]
+
         system("cls")
-        print(Fore.CYAN + "INFO O UŽIVATELI" + Fore.RESET)
+        print(Fore.CYAN + "INFO O UŽIVATELI\n" + Fore.RESET)
         print_kv("Alias", selected_user, Fore.CYAN)
         print_kv("Discord ID", user['discord_id'], Fore.CYAN)
         print_kv("Wikidot účet", user['wikidot'], Fore.CYAN)
-        print_kv("Celkem bodů", "Ano" if user['exception'] else user['total_points'], Fore.CYAN)
+        print_kv("Celkem bodů", "Ano" if user['exception'] else format(user['total_points'], '.2f'), Fore.CYAN)
+        print_kv("Poslední zapsaný překlad", last_article, Fore.CYAN)
         print(Fore.YELLOW + "\nMENU" + Fore.RESET + "\n1. Zobrazit seznam přeložených článků\n2. Přidat přeložený článek\n3. Odebrat uživatele\n4. Zpět\n")
     
     while True: 
@@ -55,9 +67,29 @@ def user_menu():
                     case 1:
                         pass
                     case 2:
-                        pass
+                        article_name = input("Jméno článku: ")
+                        article_words = input("Počet slov: ")
+                        article_bonus = input("Bonusové body: ")
+                        
+                        # U normálních SCP automaticky složit odkaz
+                        if re.match(r"^SCP-\d{3,4}(-J)?$", article_name):
+                            wdlink = "https://scp-cs.wikidot.com/" + article_name.lower()
+                        else:
+                            # Automaticky přepsat odkazy z EN na CS wiki
+                            # TODO: Funguje to ale vypadá to hrozně
+                            article_link = input("Odkaz na článek: ")
+                            wdlink = "NULL" if article_link.isspace() else article_link.replace('https://scp-wiki', 'http://scp-cs', 1).replace('https://wanderers-library', 'http://wanderers-library-cs')
+
+                        db[selected_user]['articles'][article_name] = {'word_count': int(article_words), 'bonus_points': int(article_bonus), 'wd_link': wdlink}
+
+                        db[selected_user]['total_points'] = sum(map(lambda x: int(x['word_count']) + 1000*int(x['bonus_points']), db[selected_user]['articles'].values())) / 1000
+
+                        write_db()
                     case 3:
-                        pass
+                        confirmation = input(Fore.RED + "Skutečně chcete tohoto uživatele odebrat? Pro potvrzení napište jeho Discord ID: " + Fore.RESET)
+                        if confirmation == db[selected_user]['discord_id']:
+                            del db[selected_user]
+                            write_db()
                     case 4:
                         system("cls")
                         return
@@ -75,35 +107,35 @@ if __name__ == "__main__":
     print(Fore.YELLOW + f"Otevírám soubor {filename_default}..." + Fore.RESET)
     with open(filename_default, "r+", encoding="utf-8") as dbfile:
         db = json.load(dbfile)
-        while True: 
-            print_menu()
-            inp = input("Zvolte možnost: ")
-            try:
-                a = int(inp)
-                if 0 < a < 6:
-                    match a:
-                        case 1:
-                            if(select_user()):
-                                user_menu()
-                            else:
-                                input("Pro pokračování stiskněte cokoliv")
-                                system("cls")
-                                continue;
-                            pass
-                        case 2:
-                            pass
-                        case 3:
-                            pass
-                        case 4:
-                            pass
-                        case 5:
-                            print(Fore.RED + "Sbohem, Administrátore." + Fore.RESET)
-                            exit(0)
-                            pass
-                else:
-                    system("cls")
-                    continue
-            except ValueError:
+    while True: 
+        print_menu()
+        inp = input("Zvolte možnost: ")
+        try:
+            a = int(inp)
+            if 0 < a < 6:
+                match a:
+                    case 1:
+                        if(select_user()):
+                            user_menu()
+                        else:
+                            input("Pro pokračování stiskněte cokoliv")
+                            system("cls")
+                            continue;
+                        pass
+                    case 2:
+                        pass
+                    case 3:
+                        pass
+                    case 4:
+                        pass
+                    case 5:
+                        print(Fore.RED + "Sbohem, Administrátore." + Fore.RESET)
+                        exit(0)
+                        pass
+            else:
                 system("cls")
                 continue
+        except ValueError:
+            system("cls")
+            continue
     
