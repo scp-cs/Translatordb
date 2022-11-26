6123495f0292bbbb9a91a37cfbfc7e1fef89ecd8
x10102 <x10102.ot@gmail.com>
2022-11-26 15:20:37 +0100

    Use colorama autoreset

--- Editor.py
+++ Editor.py
@@ -37,7 +37,7 @@ def write_db():
         with open(filename_default, "w", encoding="utf-8") as dbfile:
             json.dump(db, dbfile)
     except IOError as e:
-        print(Fore.RED + "Chyba při zápisu: " + str(e) + Fore.RESET)
+        print(Fore.RED + "Chyba při zápisu: " + str(e))
 
 def print_kv(key, value, color):
     print(f"{key}: {color}{value}{Fore.RESET}{Back.RESET}")
@@ -45,24 +45,30 @@ def print_kv(key, value, color):
 def print_menu():
     print(Fore.YELLOW + "\nMENU" + Fore.RESET + "\n1. Vybrat uživatele\n2. Zobrazit seznam uživatelů\n3. Přidat nového uživatele\n4. Odejít\n")
 
+def url_rewrite(url):
+    new_url = url
+    for rule in rewrite_urls.items():
+        new_url = new_url.replace(rule[0], rule[1])
+    return new_url
+
 def select_user():
     global selected_user
     name = input("Zadejte alias, DID nebo WID uživatele: ")
     if name in db.keys():
         selected_user = name
         system("cls")
-        print(Fore.GREEN + f"\nVybrán uživatel {name}." + Fore.RESET)
+        print(Fore.GREEN + f"\nVybrán uživatel {name}.")
         user_menu()
         return
     else:
         for user in db:
             if name == db[user]['discord_id'] or name == db[user]['wikidot']:
                 system("cls")
-                print(Fore.GREEN + f"\nVybrán uživatel {user}." + Fore.RESET)
+                print(Fore.GREEN + f"\nVybrán uživatel {user}.")
                 selected_user = user
                 user_menu()
                 return
-    print(Fore.RED + f'\nUživatel "{name}" nenalezen' + Fore.RESET)
+    print(Fore.RED + f'\nUživatel "{name}" nenalezen')
     readkey()
     system('cls')
 
@@ -151,7 +157,7 @@ def add_user():
             exc = False
     db[nick] = {'discord_id': did, 'wikidot': wid, 'total_points': 0 if not exc else 9999, 'role_level': 0, 'exception': exc, 'articles': {}}
     write_db()
-    print(Fore.GREEN + "Uživatel přidán. Stiskněte cokoliv pro pokračování" + Fore.RESET)
+    print(Fore.GREEN + "Uživatel přidán. Stiskněte cokoliv pro pokračování")
     readkey()
     system('cls')
 
@@ -163,7 +169,7 @@ def user_menu():
         last_article = list(user['articles'].keys())[-1] if len(user['articles']) > 0 else "ne."
 
         system("cls")
-        print(Fore.CYAN + "INFO O UŽIVATELI\n" + Fore.RESET)
+        print(Fore.CYAN + "INFO O UŽIVATELI\n")
         print_kv("Alias", selected_user, Fore.CYAN)
         print_kv("Discord ID", user['discord_id'], Fore.CYAN)
         print_kv("Wikidot účet", user['wikidot'], Fore.CYAN)
@@ -191,7 +197,7 @@ def user_menu():
                         else:
                             # Automatically rewrite EN wiki links to CS
                             article_link = input("Odkaz na článek: ")
-                            wdlink = "NULL" if article_link.isspace() else article_link.replace('https://scp-wiki', 'http://scp-cs', 1).replace('https://wanderers-library', 'http://wanderers-library-cs')
+                            wdlink = "NULL" if article_link.isspace() else url_rewrite(article_link)
 
                         db[selected_user]['articles'][article_name] = {'word_count': int(article_words), 'bonus_points': int(article_bonus), 'wd_link': wdlink}
 
@@ -199,10 +205,12 @@ def user_menu():
 
                         write_db()
                     case 3:
-                        confirmation = input(Fore.RED + "Skutečně chcete tohoto uživatele odebrat? Pro potvrzení napište jeho Discord ID: " + Fore.RESET)
+                        confirmation = input(Fore.RED + "Skutečně chcete tohoto uživatele odebrat? Pro potvrzení napište jeho Discord ID: ")
                         if confirmation == db[selected_user]['discord_id']:
                             del db[selected_user]
                             write_db()
+                        else:
+                            print(Fore.RED + "Nesprávné Discord ID.")
                     case 4:
                         system("cls")
                         return
@@ -216,9 +224,9 @@ def user_menu():
 if __name__ == "__main__":
     system("cls")
     system("title SCiPNET v1.4")
-    init()
-    print(Fore.GREEN + "Vítá vás HR databáze SCiPNETu!" + Fore.RESET)
-    print(Fore.YELLOW + f"Otevírám soubor {filename_default}..." + Fore.RESET)
+    init(autoreset=True)
+    print(Fore.GREEN + "Vítá vás HR databáze SCiPNETu!")
+    print(Fore.YELLOW + f"Otevírám soubor {filename_default}...")
     with open(filename_default, "r+", encoding="utf-8") as dbfile:
         db = json.load(dbfile)
     while True: 
@@ -232,7 +240,7 @@ if __name__ == "__main__":
                 case 2: display_user_table()
                 case 3: add_user()
                 case 4:
-                    print(Fore.RED + "Sbohem, Administrátore." + Fore.RESET)
+                    print(Fore.RED + "Sbohem, Administrátore.")
                     exit(0)
         except ValueError:
             system("cls")
